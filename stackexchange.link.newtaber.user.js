ff227d6f214358ed7729c86b837cb563da5b8687
Paola Captanovska <36765595+almaceleste@users.noreply.github.com>
2018-02-24 21:30:31 +0300

    Add files via upload
    fix: broken vote links
    add: links from comments
    add: userlinks

--- stackexchange.link.newtaber.user.js
+++ stackexchange.link.newtaber.user.js
@@ -1,33 +1,31 @@
 // ==UserScript==
-// @name         StackExchange link newtaber
-// @namespace    https://github.com/almaceleste/userscripts
-// @version      0.1
-// @description  this code opens links from posts, answers and (maybe) comments in the new tab instead of the annoying in-place opening
-// @author       (ɔ) Paola Captanovska
-// @match        https://*.stackexchange.com/questions/*
-// @match        https://*.stackoverflow.com/questions/*
-// @match        https://askubuntu.com/questions/*
-// @match        https://mathoverflow.net/questions/*
-// @match        https://serverfault.com/questions/*
-// @match        https://stackapps.com/questions/*
-// @match        https://superuser.com/questions/*
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @downloadURL  https://github.com/almaceleste/userscripts/raw/master/stackexchange.link.newtaber.user.js
-// @updateURL    https://github.com/almaceleste/userscripts/raw/master/stackexchange.link.newtaber.meta.js
-// @grant        none
+// @name            StackExchange link newtaber
+// @namespace       https://github.com/almaceleste/userscripts
+// @version         0.2
+// @description     this code opens links from posts, answers, comments and user signatures in the new tab instead of the annoying in-place opening
+// @description:ru  этот код открывает ссылки из постов, ответов, комментариев и подписей пользователей в новой вкладке вместо надоедливого открытия в текущей
+// @author          (ɔ) Paola Captanovska
+// @match           https://*.stackexchange.com/questions/*
+// @match           https://*.stackoverflow.com/questions/*
+// @match           https://askubuntu.com/questions/*
+// @match           https://mathoverflow.net/questions/*
+// @match           https://serverfault.com/questions/*
+// @match           https://stackapps.com/questions/*
+// @match           https://superuser.com/questions/*
+// @downloadURL     https://github.com/almaceleste/userscripts/raw/master/stackexchange.link.newtaber.user.js
+// @updateURL       https://github.com/almaceleste/userscripts/raw/master/stackexchange.link.newtaber.meta.js
+// @icon            https://cdn1.iconfinder.com/data/icons/feather-2/24/external-link-128.png
+// @grant           none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    $('.postcell a, .answercell a, .comment-text a').each(function() {
-        var a = new RegExp('/' + window.location.host + '/');
-        if(!a.test(this.href)) {
-            $(this).click(function(event) {
-                event.preventDefault();
-                event.stopPropagation();
-                window.open(this.href, '_blank');
-            });
-        }
+    $('.post-text a, .comment-text a, .user-details a').each(function() {
+        $(this).click(function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            window.open(this.href, '_blank');
+        });
     });
 })();
