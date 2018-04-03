f447f1a6a7ee323f9659289e542b48436dbd5182
Paola Captanovska <pcaptanovska@gmail.com>
2018-04-03 13:50:55 +0300

    Add files via upload
    initial upload

---
+++ Hola_Mundo.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name            Hola Mundo
+// @namespace       almaceleste
+// @version         0.1
+// @description     this code does nothing only writes hola message to console
+// @description:ru  этот код не делает ничего, только пишет приветственное сообщение в консоль
+// @author          (ɔ) Paola Captanovska
+// @license         AGPL-3.0+; [AGPL 3.0](http://www.gnu.org/licenses/agpl)
+// @icon            https://cdn0.iconfinder.com/data/icons/typicons-2/24/message-32.png
+// @icon64          https://cdn0.iconfinder.com/data/icons/typicons-2/24/message-128.png
+
+// @homepageURL     https://greasyfork.org/en/users/174037-almaceleste
+// @homepageURL     https://openuserjs.org/users/almaceleste
+// @homepageURL     https://github.com/almaceleste/userscripts
+// @supportURL      https://github.com/almaceleste/userscripts/issues
+// @updateURL       https://github.com/almaceleste/userscripts/raw/master/Hola_Mundo.meta.js
+// @downloadURL     https://github.com/almaceleste/userscripts/raw/master/Hola_Mundo.user.js
+// @downloadURL     https://openuserjs.org/install/almaceleste/Hola_Mundo.user.js
+
+// @match           http*://*/*
+// ==/UserScript==
+
+// ==OpenUserJS==
+// @author almaceleste
+// ==/OpenUserJS==
+
+
+(function() {
+    'use strict';
+
+    console.log('¡hola mundo!');
+})();
