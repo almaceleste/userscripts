0603a25eb3b6bbcbb80e5fad86396d6134fa0661
almaceleste <pcaptanovska@gmail.com>
2020-05-25 11:13:02 +0300

    refactored, new design, some small fixes

--- src/External_link_newtaber.user.js
+++ src/External_link_newtaber.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name            External link newtaber
 // @namespace       almaceleste
-// @version         0.2.5
-// @description     this code opens external links in new tab on all sites (now may work on dynamic lists of links such as search results)
-// @description:ru  этот код открывает внешние ссылки в новой вкладке на всех сайтах (теперь должно работать с динамическими списками ссылок, такими как результаты поисковых запросов)
+// @version         0.3.0
+// @description     opens external links in a new tab on all sites (now can work with dynamic link lists, such as search results)
+// @description:ru  открывает внешние ссылки в новой вкладке на всех сайтах (теперь должно работать с динамическими списками ссылок, такими как результаты поисковых запросов)
 // @author          (ɔ) Paola Captanovska
-// @license         AGPL-3.0; http://www.gnu.org/licenses/agpl.txt
+// @license         AGPL-3.0-or-later; http://www.gnu.org/licenses/agpl.txt
 // @icon            https://cdn1.iconfinder.com/data/icons/feather-2/24/external-link-32.png
 // @icon64          https://cdn1.iconfinder.com/data/icons/feather-2/24/external-link-128.png
 
@@ -31,8 +31,80 @@
 // @author almaceleste
 // ==/OpenUserJS==
 
-const windowcss = '#allnewtaberCfg {background-color: lightblue;} #allnewtaberCfg .reset_holder {float: left; position: relative; bottom: -1em;} #allnewtaberCfg .saveclose_buttons {margin: .7em;}';
-const iframecss = 'height: 30.1em; width: 30em; border: 1px solid; border-radius: 3px; position: fixed; z-index: 999;';
+const configId = 'allnewtaberCfg';
+const windowcss = `
+    #${configId} {
+        background-color: darkslategray;
+        color: whitesmoke;
+    }
+    #${configId} a,
+    #${configId} button,
+    #${configId} input,
+    #${configId} select,
+    #${configId} select option,
+    #${configId} .section_desc {
+        color: whitesmoke !important;
+    }
+    #${configId} a,
+    #${configId} button,
+    #${configId} input,
+    #${configId} .section_desc {
+        font-size: .8em !important;
+    }
+    #${configId} button,
+    #${configId} select,
+    #${configId} select option,
+    #${configId} .section_desc {
+        background-color: #333;
+        border: 1px solid #222;
+    }
+    #${configId} button{
+        height: 1.65em !important;
+    }
+    #${configId}_header {
+        font-size: 1.3em !important;
+    }
+    #${configId}.section_header {
+        background-color: #454545;
+        border: 1px solid #222;
+        font-size: 1em !important;
+    }
+    #${configId} .field_label {
+        font-size: .7em !important;
+    }
+    #${configId}_buttons_holder {
+        position: fixed;
+        width: 97%;
+        bottom: 0;
+    }
+    #${configId} .reset_holder {
+        float: left;
+        position: relative;
+        bottom: -1em;
+    }
+    #${configId} .saveclose_buttons {
+        margin: .7em;
+    }
+    #${configId}_field_support {
+        background: none !important;
+        border: none;
+        cursor: pointer;      
+        padding: 0 !important;
+        text-decoration: underline;
+    }
+    #${configId}_field_support:hover,
+    #${configId}_resetLink:hover {
+        filter: drop-shadow(0 0 1px dodgerblue);
+    }
+`;
+const iframecss = `
+    height: 28em;
+    width: 30em;
+    border: 1px solid;
+    border-radius: 3px;
+    position: fixed;
+    z-index: 999;
+`;
 
 var host = window.location.hostname;
 var flat = host.replace(/\..*/, '');
@@ -40,82 +112,81 @@ var root = host.replace(/^[^.]*\./, '');
 var child = '*.' + host;
 var next = '*.' + root;
 
-GM_registerMenuCommand('External link newtaber Settings', opencfg);
-
-function opencfg()
-{
+GM_registerMenuCommand(`${GM_info.script.name} Settings`, () => {
 	GM_config.open();
-	allnewtaberCfg.style = iframecss;
-}
+    GM_config.frame.style = iframecss;
+});
 
-GM_config.init(
-{
-    id: 'allnewtaberCfg',
-    title: 'External link newtaber',
-    fields:
-    {
-        level:
-        {
-            section: ['Exclusions', 'Exclude these domains (do not open in new tab)'],
+GM_config.init({
+    id: `${configId}`,
+    title: `${GM_info.script.name} ${GM_info.script.version}`,
+    fields: {
+        level: {
+            section: ['', 'Exclude these domains (do not open in new tab)'],
             label: 'do not exclude parent and neighbor sites if parent site is a root domain like .com',
             labelPos: 'right',
             type: 'checkbox',
             default: true,
         },
-        root:
-        {
+        root: {
             label: 'parent site links (' + root + ')',
             labelPos: 'right',
             type: 'checkbox',
             default: true,
         },
-        next:
-        {
+        next: {
             label: 'neighbor site links (' + next + ')',
             labelPos: 'right',
             type: 'checkbox',
             default: true,
         },
-        host:
-        {
+        host: {
             label: 'this site links (' + host + ')',
             labelPos: 'right',
             type: 'checkbox',
             default: true,
         },
-        child:
-        {
+        child: {
             label: 'child site links (' + child + ')',
             labelPos: 'right',
             type: 'checkbox',
             default: true,
         },
-        background:
-        {
-            section: ['Options', 'Other options'],
+        background: {
+            section: ['', 'Other options'],
             label: 'open new tab in background',
             labelPos: 'right',
             type: 'checkbox',
             default: false,
         },
-        insert:
-        {
+        insert: {
             label: 'insert new tab next to the current instead of the right end',
             labelPos: 'right',
             type: 'checkbox',
             default: true,
         },
-        setParent:
-        {
+        setParent: {
             label: 'return to the current tab after new tab closed',
             labelPos: 'right',
             type: 'checkbox',
             default: true,
         },
+        support: {
+            section: ['', 'Support'],
+            label: 'almaceleste.github.io',
+            title: 'more info on almaceleste.github.io',
+            type: 'button',
+            click: () => {
+                GM_openInTab('https://almaceleste.github.io', {
+                    active: true,
+                    insert: true,
+                    setParent: true
+                });
+            }
+        },
     },
     css: windowcss,
-    events:
-    {
+    events: {
         save: function() {
             GM_config.close();
         }
@@ -125,13 +196,15 @@ GM_config.init(
 (function() {
     'use strict';
 
-    var empty = new RegExp('^$');
+    const empty = new RegExp('^$');
     var patternroot = empty;
     var patternhost = empty;
     host = host.replace(/\./g, '\\\.');
     root = root.replace(/\./g, '\\\.');
-    var background = GM_config.get('background');
-    var options = {active: !GM_config.get('background'), insert: GM_config.get('insert'), setParent: GM_config.get('setParent')};
+    const background = GM_config.get('background');
+    const insert = GM_config.get('insert');
+    const setParent = GM_config.get('setParent');
+    const options = {active: !background, insert: insert, setParent: setParent};
 
     if (GM_config.get('root')){patternroot = new RegExp('^' + root + '$');}    // abc.x               => ^abc\.x$
     if (GM_config.get('next')){
@@ -149,31 +222,24 @@ GM_config.init(
         else {patternhost = new RegExp('.+\.' + host + '$');}                  // *.w.abc.x           => .+\.w\.abc\.x$
     }
 
-    // function main(){
+    window.onload = function(){
         var anchors = document.getElementsByTagName('a');
-        // alert('hola mundo');
         for (var i = 0; i < anchors.length; i++) {
             var a = anchors[i];
-            // console.log('href', a.href, a);
             var target = a.host;
             if (a.hasAttribute('href')){
                 if (target && !empty.test(target)){
                     if (!patternroot.test(target) && !patternhost.test(target)){
-                        // a.target = '_blank';
                         a.addEventListener('click', newtaber);
                     }
                 }
             }
         }
-    // }
+    }
 
     function newtaber(e){
         e.preventDefault();
         e.stopPropagation();
         GM_openInTab(this.href, options);
     }
-
-    // window.onload = main;
-    // setTimeout(main(), 1000);
-    // document.getElementById('search').addListener('load', main());
 })();
