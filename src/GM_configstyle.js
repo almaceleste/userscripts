00dee8bf0967762d18ac638748f556b019bc178b
almaceleste <pcaptanovska@gmail.com>
2020-05-26 02:29:05 +0300

    added object GM_configstyle

--- src/GM_configstyle.js
+++ src/GM_configstyle.js
@@ -24,70 +24,88 @@
 // @author almaceleste
 // ==/OpenUserJS==
 
-const id = (typeof configId === 'undefined' ) ? 'configCfg' : configId;
-const windowcss = `
-    #${id} {
-        background-color: darkslategray;
-        color: whitesmoke;
+const GM_configstyle = {};
+
+GM_configstyle.style = (style, configId) => {
+    const defaultId = (typeof configId === 'undefined') ? 'configCfg' : configId;
+    const medium = `
+        #${defaultId} {
+            background-color: darkslategray;
+            color: whitesmoke;
+        }
+        #${defaultId} a,
+        #${defaultId} button,
+        #${defaultId} input,
+        #${defaultId} select,
+        #${defaultId} select option,
+        #${defaultId} .section_desc {
+            color: whitesmoke !important;
+        }
+        #${defaultId} a,
+        #${defaultId} button,
+        #${defaultId} input,
+        #${defaultId} .section_desc {
+            font-size: .8em !important;
+        }
+        #${defaultId} button,
+        #${defaultId} input,
+        #${defaultId} select,
+        #${defaultId} select option,
+        #${defaultId} .section_desc {
+            background-color: #333;
+            border: 1px soldefaultId #222;
+        }
+        #${defaultId} button{
+            height: 1.65em !important;
+        }
+        #${defaultId}_header {
+            font-size: 1.3em !important;
+        }
+        #${defaultId}.section_header {
+            background-color: #454545;
+            border: 1px soldefaultId #222;
+            font-size: 1em !important;
+        }
+        #${defaultId} .field_label {
+            font-size: .7em !important;
+        }
+        #${defaultId}_buttons_holder {
+            position: fixed;
+            wdefaultIdth: 97%;
+            bottom: 0;
+        }
+        #${defaultId} .reset_holder {
+            float: left;
+            position: relative;
+            bottom: -1em;
+        }
+        #${defaultId} .saveclose_buttons {
+            margin: .7em;
+        }
+        #${defaultId}_field_support {
+            background: none !important;
+            border: none !important;
+            cursor: pointer !important;      
+            padding: 0 !important;
+            text-decoration: underline !important;
+        }
+        #${defaultId}_field_support:hover,
+        #${defaultId}_resetLink:hover {
+            filter: drop-shadow(0 0 1px dodgerblue);
+        }
+    `;
+    
+        switch (style) {
+        case 'medium':
+            return medium;
+            break;
+
+            default:
+            return medium;
+            break;
     }
-    #${id} a,
-    #${id} button,
-    #${id} input,
-    #${id} select,
-    #${id} select option,
-    #${id} .section_desc {
-        color: whitesmoke !important;
-    }
-    #${id} a,
-    #${id} button,
-    #${id} input,
-    #${id} .section_desc {
-        font-size: .8em !important;
-    }
-    #${id} button,
-    #${id} input,
-    #${id} select,
-    #${id} select option,
-    #${id} .section_desc {
-        background-color: #333;
-        border: 1px solid #222;
-    }
-    #${id} button{
-        height: 1.65em !important;
-    }
-    #${id}_header {
-        font-size: 1.3em !important;
-    }
-    #${id}.section_header {
-        background-color: #454545;
-        border: 1px solid #222;
-        font-size: 1em !important;
-    }
-    #${id} .field_label {
-        font-size: .7em !important;
-    }
-    #${id}_buttons_holder {
-        position: fixed;
-        width: 97%;
-        bottom: 0;
-    }
-    #${id} .reset_holder {
-        float: left;
-        position: relative;
-        bottom: -1em;
-    }
-    #${id} .saveclose_buttons {
-        margin: .7em;
-    }
-    #${id}_field_support {
-        background: none !important;
-        border: none !important;
-        cursor: pointer !important;      
-        padding: 0 !important;
-        text-decoration: underline !important;
-    }
-    #${id}_field_support:hover,
-    #${id}_resetLink:hover {
-        filter: drop-shadow(0 0 1px dodgerblue);
-    }
-`;
+};
+
+// (function(){
+
+// })();
